c272276656137d1f813ccccdaeae42e1de0ad9ee
Philipp Maier <pmaier@sysmocom.de>
2023-10-20 12:59:31 +0200

    utils: add apidoc for ipa_asn1c_consume_bytes_cb

--- src/ipa/libipa/utils.h
+++ src/ipa/libipa/utils.h
@@ -3,5 +3,5 @@
 #include <onomondo/ipa/utils.h>
 #include <stddef.h>
 
-/* TODO: internal utils go here */
+/* \! Callback function to be passed to asn1c decoder function. */
 int ipa_asn1c_consume_bytes_cb(const void *buffer, size_t size, void *priv);
