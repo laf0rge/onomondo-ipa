f77b2752b86225a05972cb8a902f1f97adafe128
Philipp Maier <pmaier@sysmocom.de>
2023-10-25 10:32:55 +0200

    ipad: define the length of an FQDN in ipad.h

--- include/onomondo/ipa/ipad.h
+++ include/onomondo/ipa/ipad.h
@@ -2,12 +2,14 @@
 
 #include <stdbool.h>
 
+#define IPA_LEN_FQDN 255
+
 struct ipa_context;
 
 /* Context for one softsim instance. */
 struct ipa_config {
-	char default_smdp_addr[1024];
-	char eim_addr[1024];
+	char default_smdp_addr[IPA_LEN_FQDN];
+	char eim_addr[IPA_LEN_FQDN];
 	bool eim_use_ssl;
 
 	/* ID number of the cardreader that interfaces the eUICC */
