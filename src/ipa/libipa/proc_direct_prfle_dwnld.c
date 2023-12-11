402403e3da8f45f3747aae61ee913b448064ac30
Harald Welte <laforge@osmocom.org>
2023-12-11 11:50:46 +0100

    cosmetic: fix typos in comment

--- src/ipa/libipa/proc_direct_prfle_dwnld.c
+++ src/ipa/libipa/proc_direct_prfle_dwnld.c
@@ -72,9 +72,9 @@ int ipa_proc_direct_prfle_dwnlod(struct ipa_context *ctx, struct ipa_proc_direct
 	}
 
 	/* TODO: At this point we should ask the user for consent before we proceed with the profile installation.
-	 * In case the user does not concent, we must abort by calling the common cancel session procedure.
-	 * (we are on an IoT device, maybe have this consent pre-set in a config file? we could als have a mechanism
-	 * where a callback is registered that is used to ask for concent?) */
+	 * In case the user does not consent, we must abort by calling the common cancel session procedure.
+	 * (we are on an IoT device, maybe have this consent pre-set in a config file? we could also have a mechanism
+	 * where a callback is registered that is used to ask for consent?) */
 
 	/* Execute sub procedure: Sub-procedure Profile Installation (See also section 3.1.3.3 of SGP.22) */
 	prfle_inst_pars.bound_profile_package = &get_bnd_prfle_pkg_res->get_bnd_prfle_pkg_ok->boundProfilePackage;
