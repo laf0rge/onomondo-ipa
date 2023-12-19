7bf338a501bf84958875a7a506a02800811fdf27
Philipp Maier <pmaier@sysmocom.de>
2023-12-19 11:47:02 +0100

    libipa: add new module for ES10a function GetEuiccConfiguredAddresses

---
+++ src/ipa/libipa/es10a_get_euicc_cfg_addr.c
@@ -0,0 +1,74 @@
+/*
+ * Author: Philipp Maier <pmaier@sysmocom.de> / sysmocom - s.f.m.c. GmbH
+ * See also: GSMA SGP.22, 5.7.3: Function (ES10a): GetEuiccConfiguredAddresses
+ *
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <onomondo/ipa/mem.h>
+#include <onomondo/ipa/utils.h>
+#include <onomondo/ipa/scard.h>
+#include <onomondo/ipa/log.h>
+#include <EuiccConfiguredAddressesRequest.h>
+#include "context.h"
+#include "utils.h"
+#include "euicc.h"
+#include "es10x.h"
+#include "es10a_get_euicc_cfg_addr.h"
+
+static int dec_get_euicc_cfg_addr(struct ipa_es10a_euicc_cfg_addr *euicc_cfg_addr, struct ipa_buf *es10a_res)
+{
+	struct EuiccConfiguredAddressesResponse *asn = NULL;
+
+	asn = ipa_es10x_res_dec(&asn_DEF_EuiccConfiguredAddressesResponse, es10a_res, "GetEuiccConfiguredAddresses");
+	if (!asn)
+		return -EINVAL;
+
+	euicc_cfg_addr->euicc_cfg_addr = asn;
+	return 0;
+}
+
+struct ipa_es10a_euicc_cfg_addr *ipa_es10a_get_euicc_cfg_addr(struct ipa_context *ctx)
+{
+	struct ipa_buf *es10a_req = NULL;
+	struct ipa_buf *es10a_res = NULL;
+	struct ipa_es10a_euicc_cfg_addr *euicc_cfg_addr = IPA_ALLOC_ZERO(struct ipa_es10a_euicc_cfg_addr);
+	struct EuiccConfiguredAddressesRequest get_euicc_cfg_addr_req = { 0 };
+	int rc;
+
+	es10a_req =
+	    ipa_es10x_req_enc(&asn_DEF_EuiccConfiguredAddressesRequest, &get_euicc_cfg_addr_req,
+			      "GetEuiccConfiguredAddresses");
+	if (!es10a_req) {
+		IPA_LOGP_ES10X("GetEuiccConfiguredAddresses", LERROR, "unable to encode ES10a request\n");
+		goto error;
+	}
+
+	es10a_res = ipa_euicc_transceive_es10x(ctx, es10a_req);
+	if (!es10a_res) {
+		IPA_LOGP_ES10X("GetEuiccConfiguredAddresses", LERROR, "no ES10a response\n");
+		goto error;
+	}
+
+	rc = dec_get_euicc_cfg_addr(euicc_cfg_addr, es10a_res);
+
+	if (rc < 0)
+		goto error;
+
+	IPA_FREE(es10a_req);
+	IPA_FREE(es10a_res);
+	return euicc_cfg_addr;
+error:
+	IPA_FREE(es10a_req);
+	IPA_FREE(es10a_res);
+	ipa_es10a_get_euicc_cfg_addr_free(euicc_cfg_addr);
+	return NULL;
+}
+
+void ipa_es10a_get_euicc_cfg_addr_free(struct ipa_es10a_euicc_cfg_addr *euicc_cfg_addr)
+{
+	IPA_ES10X_RES_FREE(asn_DEF_EuiccConfiguredAddressesResponse, euicc_cfg_addr);
+}
---
+++ src/ipa/libipa/es10a_get_euicc_cfg_addr.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <EuiccConfiguredAddressesResponse.h>
+struct ipa_context;
+
+struct ipa_es10a_euicc_cfg_addr {
+	struct EuiccConfiguredAddressesResponse *euicc_cfg_addr;
+};
+
+struct ipa_es10a_euicc_cfg_addr *ipa_es10a_get_euicc_cfg_addr(struct ipa_context *ctx);
+void ipa_es10a_get_euicc_cfg_addr_free(struct ipa_es10a_euicc_cfg_addr *euicc_cfg_addr);
