0e6988ef13164357cbd363e1bb41d031b40b6ee9
Philipp Maier <pmaier@sysmocom.de>
2023-12-21 17:31:34 +0100

    libipa: add new module for ES10b function GetEimConfigurationData

---
+++ src/ipa/libipa/es10b_get_eim_cfg_data.c
@@ -0,0 +1,74 @@
+/*
+ * Author: Philipp Maier <pmaier@sysmocom.de> / sysmocom - s.f.m.c. GmbH
+ * See also: GSMA SGP.32, 5.9.18: Function (ES10b): GetEimConfigurationData
+ *
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <onomondo/ipa/mem.h>
+#include <onomondo/ipa/utils.h>
+#include <onomondo/ipa/scard.h>
+#include <onomondo/ipa/log.h>
+#include <GetEimConfigurationDataRequest.h>
+#include "context.h"
+#include "utils.h"
+#include "euicc.h"
+#include "es10x.h"
+#include "es10b_get_eim_cfg_data.h"
+
+static int dec_get_eim_cfg_data(struct ipa_es10b_eim_cfg_data *eim_cfg_data, struct ipa_buf *es10a_res)
+{
+	struct GetEimConfigurationDataResponse *asn = NULL;
+
+	asn = ipa_es10x_res_dec(&asn_DEF_GetEimConfigurationDataResponse, es10a_res, "GetEimConfigurationData");
+	if (!asn)
+		return -EINVAL;
+
+	eim_cfg_data->eim_cfg_data = asn;
+	return 0;
+}
+
+struct ipa_es10b_eim_cfg_data *ipa_es10b_get_eim_cfg_data(struct ipa_context *ctx)
+{
+	struct ipa_buf *es10a_req = NULL;
+	struct ipa_buf *es10a_res = NULL;
+	struct ipa_es10b_eim_cfg_data *eim_cfg_data = IPA_ALLOC_ZERO(struct ipa_es10b_eim_cfg_data);
+	struct GetEimConfigurationDataRequest get_eim_cfg_data_req = { 0 };
+	int rc;
+
+	es10a_req =
+	    ipa_es10x_req_enc(&asn_DEF_GetEimConfigurationDataRequest, &get_eim_cfg_data_req,
+			      "GetEimConfigurationData");
+	if (!es10a_req) {
+		IPA_LOGP_ES10X("GetEimConfigurationData", LERROR, "unable to encode ES10b request\n");
+		goto error;
+	}
+
+	es10a_res = ipa_euicc_transceive_es10x(ctx, es10a_req);
+	if (!es10a_res) {
+		IPA_LOGP_ES10X("GetEimConfigurationData", LERROR, "no ES10b response\n");
+		goto error;
+	}
+
+	rc = dec_get_eim_cfg_data(eim_cfg_data, es10a_res);
+
+	if (rc < 0)
+		goto error;
+
+	IPA_FREE(es10a_req);
+	IPA_FREE(es10a_res);
+	return eim_cfg_data;
+error:
+	IPA_FREE(es10a_req);
+	IPA_FREE(es10a_res);
+	ipa_es10b_get_eim_cfg_data_free(eim_cfg_data);
+	return NULL;
+}
+
+void ipa_es10b_get_eim_cfg_data_free(struct ipa_es10b_eim_cfg_data *eim_cfg_data)
+{
+	IPA_ES10X_RES_FREE(asn_DEF_GetEimConfigurationDataResponse, eim_cfg_data);
+}
---
+++ src/ipa/libipa/es10b_get_eim_cfg_data.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <GetEimConfigurationDataResponse.h>
+struct ipa_context;
+
+struct ipa_es10b_eim_cfg_data {
+	struct GetEimConfigurationDataResponse *eim_cfg_data;
+};
+
+struct ipa_es10b_eim_cfg_data *ipa_es10b_get_eim_cfg_data(struct ipa_context *ctx);
+void ipa_es10b_get_eim_cfg_data_free(struct ipa_es10b_eim_cfg_data *eim_cfg_data);
