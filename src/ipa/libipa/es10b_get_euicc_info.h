bc1a8f558587d7276047efd76965aaf4415c6335
Philipp Maier <pmaier@sysmocom.de>
2023-12-21 16:59:58 +0100

    es10b_get_euicc_info: cosmetic: use struct EUICCInfo declaration

--- src/ipa/libipa/es10b_get_euicc_info.h
+++ src/ipa/libipa/es10b_get_euicc_info.h
@@ -6,8 +6,8 @@
 
 /* GSMA SGP.22, section 5.7.8 */
 struct ipa_es10b_euicc_info {
-	EUICCInfo1_t *euicc_info_1;
-	EUICCInfo2_t *euicc_info_2;
+	struct EUICCInfo1 *euicc_info_1;
+	struct EUICCInfo2 *euicc_info_2;
 };
 
 struct ipa_es10b_euicc_info *ipa_es10b_get_euicc_info(struct ipa_context *ctx, bool full);
