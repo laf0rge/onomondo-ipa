60ecc0a13fb49db1d1bac00650cef59dd32819ad
Philipp Maier <pmaier@sysmocom.de>
2024-01-02 14:44:16 +0100

    euicc: remove excess whitespace

--- src/ipa/libipa/euicc.h
+++ src/ipa/libipa/euicc.h
@@ -5,6 +5,5 @@ struct ipa_buf;
 #define IPA_EUICC_RESPONSE_BUF_SIZE 5120
 
 struct ipa_buf *ipa_euicc_transceive_es10x(struct ipa_context *ctx, const struct ipa_buf *es10x_req);
-
 int ipa_euicc_init_es10x(struct ipa_context *ctx);
 int ipa_euicc_close_es10x(struct ipa_context *ctx);
