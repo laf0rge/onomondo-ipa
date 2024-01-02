4107d8a864fead0a0fafff1dd9b190107adc0b7b
Philipp Maier <pmaier@sysmocom.de>
2024-01-02 15:06:58 +0100

    length: remove abandoned define constants

--- src/ipa/libipa/length.h
+++ src/ipa/libipa/length.h
@@ -2,8 +2,6 @@
 
 #define IPA_LIMIT_HTTP_REQ 4096
 
-#define IPA_LEN_AC 1024		/* bytes */
-#define IPA_LEN_EID 16		/* bytes */
 #define IPA_LEN_EUICC_CHLG 16	/* bytes */
 #define IPA_LEN_SERV_CHLG 16	/* bytes */
 #define IPA_LEN_EID 16		/* bytes */
