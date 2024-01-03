0175c28a4737618ad40fc014d91bef18e895832c
Philipp Maier <pmaier@sysmocom.de>
2024-01-03 10:22:18 +0100

    context: add missing apidoc

--- src/ipa/libipa/context.h
+++ src/ipa/libipa/context.h
@@ -3,10 +3,17 @@
 #include "length.h"
 struct ipa_config;
 
-/* Context for one IPA instance. */
+/*! Context for one IPAd instance. */
 struct ipa_context {
+	/*! pointer to IPAd configuration options. */
 	struct ipa_config *cfg;
+
+	/*! sub-context of the HTTP(s) connection towards the eIM, */
 	void *http_ctx;
+
+	/*! sub-context of the smartcard connection towards the eUICC, */
 	void *scard_ctx;
+
+	/*! cached eID of the eUICC. */
 	uint8_t eid[IPA_LEN_EID];
 };
