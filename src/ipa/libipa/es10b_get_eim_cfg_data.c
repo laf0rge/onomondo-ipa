84f0aebd855a529ab88081d3ef7c6f1eaf0198bd
Philipp Maier <pmaier@sysmocom.de>
2024-01-08 12:41:03 +0100

    es10b_get_eim_cfg_data: simplify access to EimConfigurationData
    The GetEimConfigurationDataResponse contains an EimConfigurationData.
    Unfortunately this list is not very easy to access due to the depth of
    the generated ASN.1 structs. Let's convert it automatically into format
    that is simpler to handle and let's also add dump functions to display
    the contents of the converted format.

--- src/ipa/libipa/es10b_get_eim_cfg_data.c
+++ src/ipa/libipa/es10b_get_eim_cfg_data.c
@@ -19,6 +19,133 @@
 #include "es10x.h"
 #include "es10b_get_eim_cfg_data.h"
 
+void convert_get_eim_cfg_data(struct ipa_es10b_eim_cfg_data *res)
+{
+	unsigned int i;
+	asn_enc_rval_t rc;
+
+	res->eim_cfg_data_list_count = res->res->eimConfigurationDataList.list.count;
+	res->eim_cfg_data_list = IPA_ALLOC_N(sizeof(struct ipa_eim_cfg_data *) * res->eim_cfg_data_list_count);
+
+	for (i = 0; i < res->eim_cfg_data_list_count; i++) {
+		res->eim_cfg_data_list[i] = IPA_ALLOC_ZERO(struct ipa_eim_cfg_data);
+
+		/* All members must be independently allocated since the caller may decide to take ownership of
+		 * res->eim_cfg_data_list before freeing struct ipa_es10b_eim_cfg_data. */
+
+		res->eim_cfg_data_list[i]->eim_id =
+		    IPA_STR_FROM_ASN(&res->res->eimConfigurationDataList.list.array[i]->eimId);
+		if (res->res->eimConfigurationDataList.list.array[i]->eimFqdn)
+			res->eim_cfg_data_list[i]->eim_fqdn =
+			    IPA_STR_FROM_ASN(res->res->eimConfigurationDataList.list.array[i]->eimFqdn);
+
+		if (res->res->eimConfigurationDataList.list.array[i]->eimIdType) {
+			res->eim_cfg_data_list[i]->eim_id_type = IPA_ALLOC(long);
+			*res->eim_cfg_data_list[i]->eim_id_type =
+			    *res->res->eimConfigurationDataList.list.array[i]->eimIdType;
+		}
+		if (res->res->eimConfigurationDataList.list.array[i]->counterValue) {
+			res->eim_cfg_data_list[i]->counter_value = IPA_ALLOC(long);
+			*res->eim_cfg_data_list[i]->counter_value =
+			    *res->res->eimConfigurationDataList.list.array[i]->counterValue;
+		}
+		if (res->res->eimConfigurationDataList.list.array[i]->associationToken) {
+			res->eim_cfg_data_list[i]->association_token = IPA_ALLOC(long);
+			*res->eim_cfg_data_list[i]->association_token =
+			    *res->res->eimConfigurationDataList.list.array[i]->associationToken;
+		}
+
+		if (res->res->eimConfigurationDataList.list.array[i]->eimPublicKeyData) {
+			switch (res->res->eimConfigurationDataList.list.array[i]->eimPublicKeyData->present) {
+			case EimConfigurationData__eimPublicKeyData_PR_eimPublicKey:
+				res->eim_cfg_data_list[i]->eim_public_key_data.eim_public_key =
+				    ipa_buf_alloc(IPA_ES10X_ASN_ENCODER_BUF_SIZE);
+				rc = der_encode(&asn_DEF_SubjectPublicKeyInfo,
+						&res->res->eimConfigurationDataList.list.array[i]->
+						eimPublicKeyData->choice.eimPublicKey, ipa_asn1c_consume_bytes_cb,
+						res->eim_cfg_data_list[i]->eim_public_key_data.eim_public_key);
+				if (rc.encoded <= 0) {
+					IPA_LOGP_ES10X("GetEimConfigurationData", LERROR,
+						       "data format conversion failed, cannot re-encode eimPublicKey in eimPublicKeyData\n");
+					IPA_FREE(res->eim_cfg_data_list[i]->eim_public_key_data.eim_public_key);
+					res->eim_cfg_data_list[i]->eim_public_key_data.eim_public_key = NULL;
+					return;
+				}
+				break;
+			case EimConfigurationData__eimPublicKeyData_PR_eimCertificate:
+				res->eim_cfg_data_list[i]->eim_public_key_data.eim_certificate =
+				    ipa_buf_alloc(IPA_ES10X_ASN_ENCODER_BUF_SIZE);
+				rc = der_encode(&asn_DEF_Certificate,
+						&res->res->eimConfigurationDataList.list.array[i]->
+						eimPublicKeyData->choice.eimCertificate, ipa_asn1c_consume_bytes_cb,
+						res->eim_cfg_data_list[i]->eim_public_key_data.eim_certificate);
+				if (rc.encoded <= 0) {
+					IPA_LOGP_ES10X("GetEimConfigurationData", LERROR,
+						       "data format conversion failed, cannot re-encode eimCertificate in eimPublicKeyData\n");
+					IPA_FREE(res->eim_cfg_data_list[i]->eim_public_key_data.eim_certificate);
+					res->eim_cfg_data_list[i]->eim_public_key_data.eim_certificate = NULL;
+					return;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (res->res->eimConfigurationDataList.list.array[i]->trustedPublicKeyDataTls) {
+			switch (res->res->eimConfigurationDataList.list.array[i]->trustedPublicKeyDataTls->present) {
+			case EimConfigurationData__trustedPublicKeyDataTls_PR_trustedEimPkTls:
+				res->eim_cfg_data_list[i]->trusted_public_key_data_tls.trusted_eim_pk_tls =
+				    ipa_buf_alloc(IPA_ES10X_ASN_ENCODER_BUF_SIZE);
+				rc = der_encode(&asn_DEF_SubjectPublicKeyInfo,
+						&res->res->eimConfigurationDataList.list.
+						array[i]->trustedPublicKeyDataTls->choice.trustedEimPkTls,
+						ipa_asn1c_consume_bytes_cb,
+						res->eim_cfg_data_list[i]->
+						trusted_public_key_data_tls.trusted_eim_pk_tls);
+				if (rc.encoded <= 0) {
+					IPA_LOGP_ES10X("GetEimConfigurationData", LERROR,
+						       "data format conversion failed, cannot re-encode trustedEimPkTls in trustedPublicKeyDataTls\n");
+					IPA_FREE(res->eim_cfg_data_list[i]->
+						 trusted_public_key_data_tls.trusted_eim_pk_tls);
+					res->eim_cfg_data_list[i]->trusted_public_key_data_tls.trusted_eim_pk_tls =
+					    NULL;
+					return;
+				}
+				break;
+			case EimConfigurationData__trustedPublicKeyDataTls_PR_trustedCertificateTls:
+				res->eim_cfg_data_list[i]->trusted_public_key_data_tls.trusted_certificate_tls =
+				    ipa_buf_alloc(IPA_ES10X_ASN_ENCODER_BUF_SIZE);
+				rc = der_encode(&asn_DEF_Certificate,
+						&res->res->eimConfigurationDataList.list.
+						array[i]->trustedPublicKeyDataTls->choice.trustedCertificateTls,
+						ipa_asn1c_consume_bytes_cb,
+						res->eim_cfg_data_list[i]->
+						trusted_public_key_data_tls.trusted_certificate_tls);
+				if (rc.encoded <= 0) {
+					IPA_LOGP_ES10X("GetEimConfigurationData", LERROR,
+						       "data format conversion failed, cannot re-encode trustedCertificateTls in trustedPublicKeyDataTls\n");
+					IPA_FREE(res->eim_cfg_data_list[i]->
+						 trusted_public_key_data_tls.trusted_certificate_tls);
+					res->eim_cfg_data_list[i]->trusted_public_key_data_tls.trusted_certificate_tls =
+					    NULL;
+					return;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
+		if (res->res->eimConfigurationDataList.list.array[i]->eimSupportedProtocol)
+			res->eim_cfg_data_list[i]->eim_supported_protocol =
+			    IPA_BUF_FROM_ASN(res->res->eimConfigurationDataList.list.array[i]->eimSupportedProtocol);
+		if (res->res->eimConfigurationDataList.list.array[i]->euiccCiPKId)
+			res->eim_cfg_data_list[i]->euicc_ci_pkid =
+			    IPA_BUF_FROM_ASN(res->res->eimConfigurationDataList.list.array[i]->euiccCiPKId);
+	}
+}
+
 static int dec_get_eim_cfg_data(struct ipa_es10b_eim_cfg_data *eim_cfg_data, const struct ipa_buf *es10a_res)
 {
 	struct GetEimConfigurationDataResponse *asn = NULL;
@@ -57,9 +184,10 @@ struct ipa_es10b_eim_cfg_data *ipa_es10b_get_eim_cfg_data(struct ipa_context *ct
 	}
 
 	rc = dec_get_eim_cfg_data(eim_cfg_data, es10a_res);
-
 	if (rc < 0)
 		goto error;
+
+	convert_get_eim_cfg_data(eim_cfg_data);
 
 	IPA_FREE(es10a_req);
 	IPA_FREE(es10a_res);
@@ -75,6 +203,10 @@ error:
  *  \param[in] res pointer to function result. */
 void ipa_es10b_get_eim_cfg_data_free(struct ipa_es10b_eim_cfg_data *res)
 {
+	if (!res)
+		return;
+
+	ipa_es10b_get_eim_cfg_data_free_list(res->eim_cfg_data_list, res->eim_cfg_data_list_count);
 	IPA_ES10X_RES_FREE(asn_DEF_GetEimConfigurationDataResponse, res);
 }
 
@@ -84,7 +216,7 @@ void ipa_es10b_get_eim_cfg_data_free(struct ipa_es10b_eim_cfg_data *res)
  *  \returns pointer to EimConfigurationData item, NULL on error. */
 struct EimConfigurationData *ipa_es10b_get_eim_cfg_data_filter(struct ipa_es10b_eim_cfg_data *res, char *eim_id)
 {
-	unsigned int i;
+	long i;
 
 	if (!res || !res->res) {
 		IPA_LOGP_ES10X("GetEimConfigurationData", LERROR,
@@ -104,8 +236,7 @@ struct EimConfigurationData *ipa_es10b_get_eim_cfg_data_filter(struct ipa_es10b_
 
 	for (i = 0; i < res->res->eimConfigurationDataList.list.count; i++) {
 		if (IPA_ASN_STR_CMP_BUF
-		    (&res->res->eimConfigurationDataList.list.array[i]->eimId, eim_id,
-		     strlen(eim_id))) {
+		    (&res->res->eimConfigurationDataList.list.array[i]->eimId, eim_id, strlen(eim_id))) {
 			return res->res->eimConfigurationDataList.list.array[i];
 		}
 	}
@@ -113,4 +244,113 @@ struct EimConfigurationData *ipa_es10b_get_eim_cfg_data_filter(struct ipa_es10b_
 	IPA_LOGP_ES10X("GetEimConfigurationData", LERROR, "cannot find eimId %s in EimConfigurationData list\n",
 		       eim_id);
 	return NULL;
+}
+
+/*! Log contents of an struct ipa_eim_cfg_data item.
+ *  \param[in] eim_cfg_data pointer to struct ipa_eim_cfg_data item.
+ *  \param[in] indent indentation level of the generated output.
+ *  \param[in] log_subsys log subsystem to generate the output for.
+ *  \param[in] log_level log level to generate the output for. */
+void ipa_es10b_get_eim_cfg_data_dump_item(struct ipa_eim_cfg_data *eim_cfg_data, uint8_t indent,
+					  enum log_subsys log_subsys, enum log_level log_level)
+{
+	char indent_str[256];
+
+	memset(indent_str, ' ', indent);
+	indent_str[indent] = '\0';
+
+	IPA_LOGP(log_subsys, log_level, "%seIM configuration: \n", indent_str);
+
+	if (!eim_cfg_data) {
+		IPA_LOGP(log_subsys, log_level, "%s (none)\n", indent_str);
+		return;
+	}
+
+	IPA_LOGP(log_subsys, log_level, "%s eimId: \"%s\"\n", indent_str, eim_cfg_data->eim_id);
+	if (eim_cfg_data->eim_fqdn)
+		IPA_LOGP(log_subsys, log_level, "%s eimFqdn: \"%s\"\n", indent_str, eim_cfg_data->eim_fqdn);
+	else
+		IPA_LOGP(log_subsys, log_level, "%s eimFqdn: (none)\n", indent_str);
+
+	if (eim_cfg_data->eim_id_type)
+		IPA_LOGP(log_subsys, log_level, "%s eimIdType: \"%ld\"\n", indent_str, *eim_cfg_data->eim_id_type);
+	else
+		IPA_LOGP(log_subsys, log_level, "%s eimIdType: (none)\n", indent_str);
+
+	if (eim_cfg_data->counter_value)
+		IPA_LOGP(log_subsys, log_level, "%s counterValue: \"%ld\"\n", indent_str, *eim_cfg_data->counter_value);
+	else
+		IPA_LOGP(log_subsys, log_level, "%s counterValue: (none)\n", indent_str);
+
+	if (eim_cfg_data->association_token)
+		IPA_LOGP(log_subsys, log_level, "%s associationToken: \"%ld\"\n", indent_str,
+			 *eim_cfg_data->association_token);
+	else
+		IPA_LOGP(log_subsys, log_level, "%s associationToken: (none)\n", indent_str);
+
+	IPA_LOGP(log_subsys, log_level, "%s eimPublicKeyData->eimPublicKey: %s\n", indent_str,
+		 ipa_buf_hexdump(eim_cfg_data->eim_public_key_data.eim_public_key));
+	IPA_LOGP(log_subsys, log_level, "%s eimPublicKeyData->eimCertificate:\n", indent_str);
+	ipa_buf_hexdump_multiline(eim_cfg_data->eim_public_key_data.eim_certificate, 32, indent + 2, log_subsys,
+				  log_level);
+
+	IPA_LOGP(log_subsys, log_level, "%s trustedPublicKeyDataTls->trustedEimPkTls: %s\n", indent_str,
+		 ipa_buf_hexdump(eim_cfg_data->trusted_public_key_data_tls.trusted_eim_pk_tls));
+	IPA_LOGP(log_subsys, log_level, "%s trustedPublicKeyDataTls->trustedCertificateTls:\n", indent_str);
+	ipa_buf_hexdump_multiline(eim_cfg_data->trusted_public_key_data_tls.trusted_certificate_tls, 32, indent + 2,
+				  log_subsys, log_level);
+
+	IPA_LOGP(log_subsys, log_level, "%s eimSupportedProtocol: %s\n", indent_str,
+		 ipa_buf_hexdump(eim_cfg_data->eim_supported_protocol));
+	IPA_LOGP(log_subsys, log_level, "%s euiccCiPKId: %s\n", indent_str,
+		 ipa_buf_hexdump(eim_cfg_data->euicc_ci_pkid));
+}
+
+/*! Log contents of eim_cfg_data_list in struct ipa_es10b_eim_cfg_data.
+ *  \param[in] eim_cfg_data_list pointer to pointer array that holds the list items.
+ *  \param[in] eim_cfg_data_list_count number of list items.
+ *  \param[in] indent indentation level of the generated output.
+ *  \param[in] log_subsys log subsystem to generate the output for.
+ *  \param[in] log_level log level to generate the output for. */
+void ipa_es10b_get_eim_cfg_data_dump_list(struct ipa_eim_cfg_data **eim_cfg_data_list, long eim_cfg_data_list_count,
+					  uint8_t indent, enum log_subsys log_subsys, enum log_level log_level)
+{
+	unsigned int i;
+	for (i = 0; i < eim_cfg_data_list_count; i++) {
+		ipa_es10b_get_eim_cfg_data_dump_item(eim_cfg_data_list[i], indent, log_subsys, log_level);
+	}
+}
+
+/*! Free list of struct ipa_eim_cfg_data.
+ *  \param[in] eim_cfg_data_list pointer to pointer array that holds the list items.
+ *  \param[in] eim_cfg_data_list_count number of list items. */
+void ipa_es10b_get_eim_cfg_data_free_list(struct ipa_eim_cfg_data **eim_cfg_data_list, long eim_cfg_data_list_count)
+{
+	long i;
+
+	if (!eim_cfg_data_list)
+		return;
+	if (!eim_cfg_data_list_count)
+		return;
+
+	for (i = 0; i < eim_cfg_data_list_count; i++) {
+		IPA_FREE(eim_cfg_data_list[i]->eim_id);
+		IPA_FREE(eim_cfg_data_list[i]->eim_fqdn);
+
+		IPA_FREE(eim_cfg_data_list[i]->eim_id_type);
+		IPA_FREE(eim_cfg_data_list[i]->counter_value);
+		IPA_FREE(eim_cfg_data_list[i]->association_token);
+
+		IPA_FREE(eim_cfg_data_list[i]->eim_public_key_data.eim_public_key);
+		IPA_FREE(eim_cfg_data_list[i]->eim_public_key_data.eim_certificate);
+
+		IPA_FREE(eim_cfg_data_list[i]->trusted_public_key_data_tls.trusted_eim_pk_tls);
+		IPA_FREE(eim_cfg_data_list[i]->trusted_public_key_data_tls.trusted_certificate_tls);
+
+		IPA_FREE(eim_cfg_data_list[i]->eim_supported_protocol);
+		IPA_FREE(eim_cfg_data_list[i]->euicc_ci_pkid);
+
+		IPA_FREE(eim_cfg_data_list[i]);
+	}
+	IPA_FREE(eim_cfg_data_list);
 }
--- src/ipa/libipa/es10b_get_eim_cfg_data.h
+++ src/ipa/libipa/es10b_get_eim_cfg_data.h
@@ -2,11 +2,45 @@
 
 #include <GetEimConfigurationDataResponse.h>
 struct ipa_context;
+struct ipa_buf;
+
+/*! Struct to hold a single item of the EimConfigurationData list */
+struct ipa_eim_cfg_data {
+	char *eim_id;
+	char *eim_fqdn;
+	long *eim_id_type;
+	long *counter_value;
+	long *association_token;
+	struct {
+		struct ipa_buf *eim_public_key;
+		struct ipa_buf *eim_certificate;
+	} eim_public_key_data;
+	struct {
+		struct ipa_buf *trusted_eim_pk_tls;
+		struct ipa_buf *trusted_certificate_tls;
+	} trusted_public_key_data_tls;
+	struct ipa_buf *eim_supported_protocol;
+	struct ipa_buf *euicc_ci_pkid;
+};
 
 struct ipa_es10b_eim_cfg_data {
 	struct GetEimConfigurationDataResponse *res;
+
+	/* The GetEimConfigurationDataResponse contains a list of EimConfigurationData elements. To simplify the
+	 * the access to the list items and their members, the list is automatically converted into an array of
+	 * struct ipa_eim_cfg_data items (see above). In case the caller wants to take ownership of eim_cfg_data_list,
+	 * he can do so by setting eim_cfg_data_list to NULL before calling ipa_es10b_get_eim_cfg_data_free. */
+	struct ipa_eim_cfg_data **eim_cfg_data_list;
+	long eim_cfg_data_list_count;
 };
 
 struct ipa_es10b_eim_cfg_data *ipa_es10b_get_eim_cfg_data(struct ipa_context *ctx);
 void ipa_es10b_get_eim_cfg_data_free(struct ipa_es10b_eim_cfg_data *res);
+
 struct EimConfigurationData *ipa_es10b_get_eim_cfg_data_filter(struct ipa_es10b_eim_cfg_data *res, char *eim_id);
+
+void ipa_es10b_get_eim_cfg_data_dump_item(struct ipa_eim_cfg_data *eim_cfg_data, uint8_t indent,
+					  enum log_subsys log_subsys, enum log_level log_level);
+void ipa_es10b_get_eim_cfg_data_dump_list(struct ipa_eim_cfg_data **eim_cfg_data_list, long eim_cfg_data_list_count,
+					  uint8_t indent, enum log_subsys log_subsys, enum log_level log_level);
+void ipa_es10b_get_eim_cfg_data_free_list(struct ipa_eim_cfg_data **eim_cfg_data_list, long eim_cfg_data_list_count);
