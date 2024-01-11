laf0rge/onomondo-ipa

commit 78c827fc49d36c2019ed70f9f262610fec645fe3

README.md: describe how to build and use the project

include/onomondo/ipa/ipad.h
@@ -31,4 +31,5 @@ struct ipa_config {
 
 struct ipa_context *ipa_new_ctx(struct ipa_config *cfg);
 int ipa_poll(struct ipa_context *ctx);
+int ipa_read_eim_cfg(struct ipa_context *ctx);
 void ipa_free_ctx(struct ipa_context *ctx);

src/ipa/libipa/Makefile.am
@@ -42,6 +42,7 @@ libipa_a_SOURCES = \
 	euicc.c \
 	ipad.c \
 	log.c \
+	proc_add_init_eim_cfg.c \
 	proc_cmn_cancel_sess.c \
 	proc_cmn_mtl_auth.c \
 	proc_direct_prfle_dwnld.c \
@@ -80,6 +81,7 @@ noinst_HEADERS = \
 	esipa_prvde_eim_pkg_rslt.h \
 	euicc.h \
 	length.h \
+	proc_add_init_eim_cfg.h \
 	proc_cmn_cancel_sess.h \
 	proc_cmn_mtl_auth.h \
 	proc_direct_prfle_dwnld.h \

src/ipa/libipa/ipad.c
@@ -17,7 +17,7 @@
 #include "euicc.h"
 #include "es10c_get_eid.h"
 #include "proc_eim_pkg_retr.h"
-
+#include "proc_add_init_eim_cfg.h"
 #include "es10b_get_eim_cfg_data.h"
 
 /*! Create a new ipa_context and prepare links towards eIM and eUICC.
@@ -79,6 +79,13 @@ error:
 int ipa_poll(struct ipa_context *ctx)
 {
 	return ipa_proc_eim_pkg_retr(ctx);
+}
+
+int ipa_read_eim_cfg(struct ipa_context *ctx)
+{
+	printf("===========> HELLO!\n");
+	ipa_proc_add_init_eim_cfg_readback(ctx);
+	return 0;
 }
 
 /*! close links towards eIM and eUICC and free an ipa_context.

src/ipa/main.c
@@ -24,6 +24,7 @@ static void print_help(void)
 	printf(" -e eimId ............ set preferred eIM (in case the eUICC has multiple)\n");
 	printf(" -r N ................ set reader number (default: %d)\n", DEFAULT_READER_NUMBER);
 	printf(" -c N ................ set logical channel number (default: %d)\n", DEFAULT_CHANNEL_NUMBER);
+	printf(" -m .................. read current eIM configuration from eUICC\n");
 }
 
 int main(int argc, char **argv)
@@ -31,6 +32,7 @@ int main(int argc, char **argv)
 	struct ipa_config cfg = { 0 };
 	struct ipa_context *ctx;
 	int opt;
+	bool read_eim_cfg = false;
 
 	printf("IPAd!\n");
 
@@ -41,7 +43,7 @@ int main(int argc, char **argv)
 
 	/* Overwrite configuration values with user defined parameters */
 	while (1) {
-		opt = getopt(argc, argv, "ht:e:r:c:S");
+		opt = getopt(argc, argv, "ht:e:r:c:Sm");
 		if (opt == -1)
 			break;
 
@@ -64,6 +66,8 @@ int main(int argc, char **argv)
 			break;
 		case 'S':
 			cfg.eim_disable_ssl = true;
+		case 'm':
+			read_eim_cfg = true;
 			break;
 		default:
 			printf("unhandled option: %c!\n", opt);
@@ -73,7 +77,7 @@ int main(int argc, char **argv)
 
 	/* Display current config */
 	printf("config:\n");
-	printf(" preferred_eim_id = %s\n", cfg.preferred_eim_id ? cfg.preferred_eim_id : "(first configured eIM)" );
+	printf(" preferred_eim_id = %s\n", cfg.preferred_eim_id ? cfg.preferred_eim_id : "(first configured eIM)");
 	printf(" reader_num = %d\n", cfg.reader_num);
 	printf(" euicc_channel = %d\n", cfg.euicc_channel);
 	printf(" eim_disable_ssl = %d\n", cfg.eim_disable_ssl);
@@ -86,8 +90,13 @@ int main(int argc, char **argv)
 		return -EINVAL;
 	}
 
-	/* Run a single poll cycle and exit. */
-	ipa_poll(ctx);
+	if (read_eim_cfg) {
+		ipa_read_eim_cfg(ctx);
+	} else {
+		/* Run a single poll cycle and exit. */
+		ipa_poll(ctx);
+	}
+
 	ipa_free_ctx(ctx);
 	return 0;
 }

commit 61020550c935de3587bcf26b427d601ad3174f0b

libipa: revert incomplete changes
The commit 78c827fc49d36c2019ed70f9f262610fec645fe3 accidentally adds
some WIP changes that shouldn't be added yet.

include/onomondo/ipa/ipad.h
@@ -31,5 +31,4 @@ struct ipa_config {
 
 struct ipa_context *ipa_new_ctx(struct ipa_config *cfg);
 int ipa_poll(struct ipa_context *ctx);
-int ipa_read_eim_cfg(struct ipa_context *ctx);
 void ipa_free_ctx(struct ipa_context *ctx);

src/ipa/libipa/Makefile.am
@@ -42,7 +42,6 @@ libipa_a_SOURCES = \
 	euicc.c \
 	ipad.c \
 	log.c \
-	proc_add_init_eim_cfg.c \
 	proc_cmn_cancel_sess.c \
 	proc_cmn_mtl_auth.c \
 	proc_direct_prfle_dwnld.c \
@@ -81,7 +80,6 @@ noinst_HEADERS = \
 	esipa_prvde_eim_pkg_rslt.h \
 	euicc.h \
 	length.h \
-	proc_add_init_eim_cfg.h \
 	proc_cmn_cancel_sess.h \
 	proc_cmn_mtl_auth.h \
 	proc_direct_prfle_dwnld.h \

src/ipa/libipa/ipad.c
@@ -17,7 +17,7 @@
 #include "euicc.h"
 #include "es10c_get_eid.h"
 #include "proc_eim_pkg_retr.h"
-#include "proc_add_init_eim_cfg.h"
+
 #include "es10b_get_eim_cfg_data.h"
 
 /*! Create a new ipa_context and prepare links towards eIM and eUICC.
@@ -79,13 +79,6 @@ error:
 int ipa_poll(struct ipa_context *ctx)
 {
 	return ipa_proc_eim_pkg_retr(ctx);
-}
-
-int ipa_read_eim_cfg(struct ipa_context *ctx)
-{
-	printf("===========> HELLO!\n");
-	ipa_proc_add_init_eim_cfg_readback(ctx);
-	return 0;
 }
 
 /*! close links towards eIM and eUICC and free an ipa_context.

src/ipa/main.c
@@ -24,7 +24,6 @@ static void print_help(void)
 	printf(" -e eimId ............ set preferred eIM (in case the eUICC has multiple)\n");
 	printf(" -r N ................ set reader number (default: %d)\n", DEFAULT_READER_NUMBER);
 	printf(" -c N ................ set logical channel number (default: %d)\n", DEFAULT_CHANNEL_NUMBER);
-	printf(" -m .................. read current eIM configuration from eUICC\n");
 }
 
 int main(int argc, char **argv)
@@ -32,7 +31,6 @@ int main(int argc, char **argv)
 	struct ipa_config cfg = { 0 };
 	struct ipa_context *ctx;
 	int opt;
-	bool read_eim_cfg = false;
 
 	printf("IPAd!\n");
 
@@ -43,7 +41,7 @@ int main(int argc, char **argv)
 
 	/* Overwrite configuration values with user defined parameters */
 	while (1) {
-		opt = getopt(argc, argv, "ht:e:r:c:Sm");
+		opt = getopt(argc, argv, "ht:e:r:c:S");
 		if (opt == -1)
 			break;
 
@@ -66,8 +64,6 @@ int main(int argc, char **argv)
 			break;
 		case 'S':
 			cfg.eim_disable_ssl = true;
-		case 'm':
-			read_eim_cfg = true;
 			break;
 		default:
 			printf("unhandled option: %c!\n", opt);
@@ -77,7 +73,7 @@ int main(int argc, char **argv)
 
 	/* Display current config */
 	printf("config:\n");
-	printf(" preferred_eim_id = %s\n", cfg.preferred_eim_id ? cfg.preferred_eim_id : "(first configured eIM)");
+	printf(" preferred_eim_id = %s\n", cfg.preferred_eim_id ? cfg.preferred_eim_id : "(first configured eIM)" );
 	printf(" reader_num = %d\n", cfg.reader_num);
 	printf(" euicc_channel = %d\n", cfg.euicc_channel);
 	printf(" eim_disable_ssl = %d\n", cfg.eim_disable_ssl);
@@ -90,13 +86,8 @@ int main(int argc, char **argv)
 		return -EINVAL;
 	}
 
-	if (read_eim_cfg) {
-		ipa_read_eim_cfg(ctx);
-	} else {
-		/* Run a single poll cycle and exit. */
-		ipa_poll(ctx);
-	}
-
+	/* Run a single poll cycle and exit. */
+	ipa_poll(ctx);
 	ipa_free_ctx(ctx);
 	return 0;
 }