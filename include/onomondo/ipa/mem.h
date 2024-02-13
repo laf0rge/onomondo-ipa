52a760526bbc0d08cc5b275076d72eac62a20064
Philipp Maier <pmaier@sysmocom.de>
2024-02-13 17:35:59 +0100

    mem: remove colons after macro definition

--- include/onomondo/ipa/mem.h
+++ include/onomondo/ipa/mem.h
@@ -2,7 +2,7 @@
 
 #include <stdlib.h>
 
-#define IPA_ALLOC(obj) malloc(sizeof(obj));
-#define IPA_ALLOC_N(n) malloc(n);
+#define IPA_ALLOC(obj) malloc(sizeof(obj))
+#define IPA_ALLOC_N(n) malloc(n)
 #define IPA_FREE(obj) free(obj)
 #define IPA_REALLOC(obj, n) realloc(obj, n)
