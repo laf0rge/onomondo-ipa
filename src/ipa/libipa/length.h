0b7cfb80954ec2a9a957e712f8e3364dd558d7c1
Philipp Maier <pmaier@sysmocom.de>
2024-02-14 11:04:08 +0100

    libipa/length: fix sourcecode formatting

--- src/ipa/libipa/length.h
+++ src/ipa/libipa/length.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#define IPA_LIMIT_HTTP_REQ 102400 /* 100kb */
+#define IPA_LIMIT_HTTP_REQ 102400	/* 100kb */
 
 #define IPA_LEN_EUICC_CHLG 16	/* bytes */
 #define IPA_LEN_SERV_CHLG 16	/* bytes */
 #define IPA_LEN_EID 16		/* bytes */
 
 /* This is the initial buffer size. The ASN.1 encoder will automatically re-alloc more memory if needed. */
-#define IPA_LEN_ASN1_ENCODER_BUF 5120 /* bytes */
+#define IPA_LEN_ASN1_ENCODER_BUF 5120	/* bytes */
 
 /* This is the initial buffer size. The ASN.1 printer will automatically re-alloc more memory if needed. */
 #define IPA_LEN_ASN1_PRINTER_BUF 10240
