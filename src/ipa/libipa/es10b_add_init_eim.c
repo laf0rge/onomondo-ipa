a7192e75e771e4c7fd7263071cb002478197c8eb
Philipp Maier <pmaier@sysmocom.de>
2024-02-15 11:51:04 +0100

    libipa/es10b_add_init_eim: add IoT eUICC emulation
    this patch adds IoT eUICC emulation to the AddInitialEim function. The
    emulation is only a simple implementation. It essentially just takes the
    presented data and copies it to the memory location where the eIM
    configuration is stored.

--- src/ipa/libipa/es10b_add_init_eim.c
+++ src/ipa/libipa/es10b_add_init_eim.c
@@ -55,12 +55,8 @@ static int dec_add_init_eim_res(struct ipa_es10b_add_init_eim_res *res, const st
 	return 0;
 }
 
-/*! Function (ES10b): AddInitialEim.
- *  \param[inout] ctx pointer to ipa_context.
- *  \param[in] req pointer to struct that holds the function parameters.
- *  \returns pointer newly allocated struct with function result, NULL on error. */
-struct ipa_es10b_add_init_eim_res *ipa_es10b_add_init_eim(struct ipa_context *ctx,
-							  const struct ipa_es10b_add_init_eim_req *req)
+static struct ipa_es10b_add_init_eim_res *add_init_eim(struct ipa_context *ctx,
+						       const struct ipa_es10b_add_init_eim_req *req)
 {
 	struct ipa_buf *es10b_req = NULL;
 	struct ipa_buf *es10b_res = NULL;
@@ -91,6 +87,54 @@ error:
 	IPA_FREE(es10b_res);
 	ipa_es10b_add_init_eim_res_free(res);
 	return NULL;
+}
+
+static struct ipa_es10b_add_init_eim_res *add_init_eim_iot_emu(struct ipa_context *ctx,
+							       const struct ipa_es10b_add_init_eim_req *req)
+{
+	struct ipa_buf *eim_cfg_new = NULL;
+	struct ipa_es10b_add_init_eim_res *res = IPA_ALLOC_ZERO(struct ipa_es10b_add_init_eim_res);
+	int rc;
+
+	/* TODO: validate (and modify if necessary) the input data as described in SGP.32, section 5.9.4 */
+
+	IPA_LOGP_ES10X("AddInitialEim", LINFO,
+		       "IoT eUICC emulation active, pretending to query eUICC to set eIM configuration...\n");
+
+	eim_cfg_new = ipa_es10x_req_enc(&asn_DEF_AddInitialEimRequest, &req->req, "AddInitialEim");
+	if (!eim_cfg_new) {
+		IPA_LOGP_ES10X("AddInitialEim", LERROR, "unable to encode ES10b request\n");
+		goto error;
+	}
+
+	/* AddInitialEimRequest and GetEimConfigurationDataResponse are identical. This means we can cast
+	 * AddInitialEimRequest encoded ASN.1 data to GetEimConfigurationDataResponse */
+	eim_cfg_new->data[1] = 0x55;
+
+	/* Replace the current eIM configuration with the new eIM configuration */
+	IPA_FREE(ctx->iot_euicc_emu.eim_cfg_ber);
+	ctx->iot_euicc_emu.eim_cfg_ber = eim_cfg_new;
+	IPA_LOGP_ES10X("AddInitialEim", LINFO, "done, eIM configuration stored in memory.\n");
+
+	/* TODO: fill in an appropriate response */
+	return res;
+error:
+	IPA_FREE(eim_cfg_new);
+	ipa_es10b_add_init_eim_res_free(res);
+	return NULL;
+}
+
+/*! Function (ES10b): AddInitialEim.
+ *  \param[inout] ctx pointer to ipa_context.
+ *  \param[in] req pointer to struct that holds the function parameters.
+ *  \returns pointer newly allocated struct with function result, NULL on error. */
+struct ipa_es10b_add_init_eim_res *ipa_es10b_add_init_eim(struct ipa_context *ctx,
+							  const struct ipa_es10b_add_init_eim_req *req)
+{
+	if (ctx->cfg->iot_euicc_emu.enabled)
+		return add_init_eim_iot_emu(ctx, req);
+	else
+		return add_init_eim(ctx, req);
 }
 
 /*! Free results of function (ES10b): AddInitialEim.
