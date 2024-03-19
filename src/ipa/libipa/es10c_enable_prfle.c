5e0602aec811a5274d7c34643b246e70c16c1e55
Philipp Maier <pmaier@sysmocom.de>
2024-03-19 17:52:05 +0100

    es10c_enable_prfle: fix typo

--- src/ipa/libipa/es10c_enable_prfle.c
+++ src/ipa/libipa/es10c_enable_prfle.c
@@ -1,6 +1,6 @@
 /*
  * Author: Philipp Maier <pmaier@sysmocom.de> / sysmocom - s.f.m.c. GmbH
- * See also: GSMA SGP.22, 5.7.16: Function (ES10b): EnableProfile
+ * See also: GSMA SGP.22, 5.7.16: Function (ES10c): EnableProfile
  *
  */
 
