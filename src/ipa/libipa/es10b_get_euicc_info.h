6d10fd33eeaa82f5aba694378f0fbb4053565746
Philipp Maier <pmaier@sysmocom.de>
2024-06-04 15:23:29 +0200

    es10b_get_euicc_info: improve comment
    Related: SYS#6690

--- src/ipa/libipa/es10b_get_euicc_info.h
+++ src/ipa/libipa/es10b_get_euicc_info.h
@@ -11,11 +11,11 @@ struct ipa_es10b_euicc_info {
 	struct EUICCInfo2 *euicc_info_2;
 	struct SGP32_EUICCInfo2 *sgp32_euicc_info_2;
 
-	/*! When the IoT eUICC emulation is enabled this function retrieve the native euicc_info_2 from the eUICC and
-	 *  convert it into sgp32_euicc_info_2. When the emulation is turned off sgp32_euicc_info_2 will be retrieved
-	 *  and no conversion is needed. This also means that euicc_info_2 will be unpopulated (NULL) in this case.
-	 *  It is recommended to use sgp32_euicc_info_2 only, since it will always be populated and technially the
-	 *  only one needed in SGP.32 / ESipa context. */
+	/*! When the IoT eUICC emulation is enabled, this function will retrieve the euicc_info_2 (SGP.22) from the
+	 *  eUICC and derive sgp32_euicc_info_2 from it. When the emulation is turned off sgp32_euicc_info_2 will
+	 *  be retrieved directly and no conversion is needed. This also means that euicc_info_2 (SGP.22) will be left
+	 *  unpopulated (NULL) in this case. Hence it is recommended to use sgp32_euicc_info_2 only, since it will
+	 *  always be populated, regardless of which eUICC type is used. */
 };
 
 struct ipa_es10b_euicc_info *ipa_es10b_get_euicc_info(struct ipa_context *ctx, bool full);
