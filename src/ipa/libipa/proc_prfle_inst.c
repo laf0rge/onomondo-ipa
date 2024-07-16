8a1488e888d51487cfe740fc8b8a719f65f81fc4
Philipp Maier <pmaier@sysmocom.de>
2024-07-16 17:02:45 +0200

    proc_prfle_inst: fix failure behavior
    When the profile download fails, the failure mode may be that the
    eUICC aborts the installation and a notification is sent, just as
    if the download were successful. In this case we must not execute
    the cancelSession procdedure (it would fail since the session
    has already ended normally with the notification). We also do not
    check the return value of ipa_bpp_segments_encode, which is a
    potential null pointer dereference. We also should check if we got
    a valid sequence number when removing the notification.
    
    Related: SYS#6564

--- src/ipa/libipa/proc_prfle_inst.c
+++ src/ipa/libipa/proc_prfle_inst.c
@@ -61,10 +61,13 @@ int ipa_proc_prfle_inst(struct ipa_context *ctx, const struct ipa_proc_prfle_ins
 	struct ipa_bpp_segments *segments = NULL;
 	unsigned int i;
 	int rc;
-	long seq_number = 0;
+	long seq_number = -1;
+	bool sucess = true;
 
 	/* Step #3-#5 Split BPP into ES8+ segments and send the segments to eUICC */
 	segments = ipa_bpp_segments_encode(pars->bound_profile_package);
+	if (!segments)
+		goto error;
 	for (i = 0; i < segments->count; i++) {
 		IPA_LOGP(SIPA, LDEBUG, "transferring ES8+ segments...\n");
 		load_bnd_prfle_pkg_res =
@@ -80,18 +83,27 @@ int ipa_proc_prfle_inst(struct ipa_context *ctx, const struct ipa_proc_prfle_ins
 			IPA_LOGP(SIPA, LERROR, "eUICC didn't respond with ProfileInstallationResult!\n");
 			goto error;
 		} else if (rc == 1 && i != segments->count - 1) {
-			IPA_LOGP(SIPA, LERROR, "profile installation aborted by eUICC!\n");
-			goto error;
+			IPA_LOGP(SIPA, LERROR, "profile installation aborted by eUICC, notfication sent!\n");
+			sucess = false;
+			break;
 		}
 	}
 
 	/* Step #11 (ES10b.RemoveNotificationFromList) */
-	rc = ipa_es10b_rm_notif_from_lst(ctx, seq_number);
-	if (rc < 0)
+	if (seq_number >= 0) {
+		if (ipa_es10b_rm_notif_from_lst(ctx, seq_number) < 0)
+			goto error;
+	} else {
+		/* We do not have a valid sequence number, either the loop above did not run at least once (0 segments)
+		 * or something else went wrong. */
 		goto error;
+	}
 
 	ipa_bpp_segments_free(segments);
-	IPA_LOGP(SIPA, LINFO, "profile installation succeeded!\n");
+	if (sucess)
+		IPA_LOGP(SIPA, LINFO, "profile installation succeeded!\n");
+	else
+		IPA_LOGP(SIPA, LINFO, "profile installation aborted!\n");
 	return 0;
 error:
 	ipa_bpp_segments_free(segments);
